dc133bb3ad42b6862066d09408c1beaaa2b518cd
Archawin Tirugsapun <zepalzth@gmail.com>
2017-05-04 00:44:18 +0700

    strategy

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -1,14 +1,11 @@
 package coinpurse;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import conipurse.strategy.GreedyWithdraw;
 import conipurse.strategy.RecursiveWithdraw;
 import gui.PurseBalanceObserver;
-import gui.PurseObserver;
+import gui.PurseListModel;
 import gui.PurseStatusObserver;
 
 /**
@@ -27,45 +24,36 @@ public class Main {
 	 *            not used
 	 */
 	public static void main(String[] args) {
-		// ResourceBundle bundle = ResourceBundle.getBundle("purse");
-		// String factoryclass = bundle.getString("moneyfactory");
-		// MoneyFactory factory = null;
-		// try {
-		// factory = (MoneyFactory) Class.forName(factoryclass).newInstance();
-		// } catch (ClassCastException cce) {
-		// System.out.println(factoryclass + " is not type MoneyFactory.");
-		// } catch (Exception ex) {
-		// System.out
-		// .println("Error creating MoneyFactory " + ex.getMessage());
-		// }
-		// MoneyFactory.setMoneyFactory(factory);
-		//
-		// Purse purse = new Purse(CAPACITY);
-		// ConsoleDialog ui = new ConsoleDialog(purse);
-		// GreedyWithdraw greedy = new GreedyWithdraw();
-		// RecursiveWithdraw recur = new RecursiveWithdraw();
-		// purse.setStrategy(recur);
-		//
-		// PurseObserver observer = new PurseObserver();
-		// PurseBalanceObserver balanceObserver = new PurseBalanceObserver();
-		// PurseStatusObserver statusObserver = new PurseStatusObserver();
-		//
-		// purse.addObserver(observer);
-		// purse.addObserver(balanceObserver);
-		// purse.addObserver(statusObserver);
-		//
-		// observer.run();
-		// balanceObserver.run();
-		// statusObserver.run();
-		// ui.run();
-		RecursiveWithdraw recursiveWithdraw = new RecursiveWithdraw();
-		List<Valuable> money = new ArrayList<Valuable>();
-		money.add(new Coin(10));
-		money.add(new Coin(5));
-		money.add(new Coin(2));
-		money.add(new Coin(2));
-		money.add(new Coin(2));
-		System.out.println(Arrays.toString(recursiveWithdraw
-				.withdraw(11, money).toArray()));
+		ResourceBundle bundle = ResourceBundle.getBundle("purse");
+		String factoryclass = bundle.getString("moneyfactory");
+		MoneyFactory factory = null;
+		try {
+			factory = (MoneyFactory) Class.forName(factoryclass).newInstance();
+		} catch (ClassCastException cce) {
+			System.out.println(factoryclass + " is not type MoneyFactory.");
+		} catch (Exception ex) {
+			System.out
+					.println("Error creating MoneyFactory " + ex.getMessage());
+		}
+		MoneyFactory.setMoneyFactory(factory);
+
+		Purse purse = new Purse(CAPACITY);
+		ConsoleDialog ui = new ConsoleDialog(purse);
+		GreedyWithdraw greedy = new GreedyWithdraw();
+		RecursiveWithdraw recur = new RecursiveWithdraw();
+		purse.setStrategy(recur);
+
+		PurseBalanceObserver balanceObserver = new PurseBalanceObserver();
+		PurseStatusObserver statusObserver = new PurseStatusObserver();
+		PurseListModel listmodel = new PurseListModel(purse);
+
+		purse.addObserver(listmodel);
+		purse.addObserver(balanceObserver);
+		purse.addObserver(statusObserver);
+
+		listmodel.run();
+		balanceObserver.run();
+		statusObserver.run();
+		ui.run();
 	}
 }
--- src/gui/PurseListModel.java
+++ src/gui/PurseListModel.java
@@ -1,16 +1,41 @@
-package coinpurse;
+package gui;
 
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.AbstractListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import coinpurse.Purse;
+import coinpurse.Valuable;
 
 public class PurseListModel extends AbstractListModel<Valuable> implements Observer {
 	List<Valuable> money;
+	JList<Valuable> listview;
+	JFrame frame;
 
 	public PurseListModel(Purse purse) {
 		this.money = purse.purseList();
+		frame = new JFrame("List Model");
+		frame.setLocation(0, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public void initcomponents() {
+		listview = new JList<>(this);
+		JScrollPane scroll = new JScrollPane(listview);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+		frame.add(scroll);
+		frame.pack();
+	}
+
+	public void run() {
+		initcomponents();
+		frame.setVisible(true);
 	}
 
 	@Override
